3f35813bfa1279a61b99655e29622c4d9c9de0dc
h3dePL <adamcygan1985@gmail.com>
2026-09-16 15:51:18 +0200

    all test pased

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const currentState = Object.assign({}, state);
+  const clonedStates = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      Object.assign(currentState, action.extraData);
+      clonedStates.push(Object.assign({}, currentState));
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete currentState[key];
+      }
+      clonedStates.push(Object.assign({}, currentState));
+    } else if (action.type === 'clear') {
+      for (const key of Object.keys(currentState)) {
+        delete currentState[key];
+      }
+      clonedStates.push(Object.assign({}, currentState));
+    }
+  }
+
+  return clonedStates;
 }
 
 module.exports = transformStateWithClones;
